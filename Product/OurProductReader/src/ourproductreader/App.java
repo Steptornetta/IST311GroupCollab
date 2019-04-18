8f1c80988a5dc31a29347d1fbac4694098ae9dbb
Steve Tornetta <steptornetta@yahoo.com>
2019-04-18 14:41:14 -0400

    Adding the NetBeans Project.

---
+++ Product/OurProductReader/src/ourproductreader/App.java
@@ -0,0 +1,18 @@
+// Stephen Tornetta PSU 2019. Java Main Class.
+
+package ourproductreader;
+
+/**
+ *
+ * @author stept
+ */
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
