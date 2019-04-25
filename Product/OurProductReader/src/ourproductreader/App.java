d9e4e8ce5644f367fd261b3ba9286ddd1547a55f
Steve Tornetta <steptornetta@yahoo.com>
2019-04-25 13:35:04 -0400

    Added Company and Team Info file reader

--- Product/OurProductReader/src/ourproductreader/App.java
+++ Product/OurProductReader/src/ourproductreader/App.java
@@ -2,6 +2,12 @@
 
 package ourproductreader;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+
 /**
  *
  * @author stept
@@ -11,14 +17,24 @@ public class App {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
         // Here is some code
+        File file = new File("Team Member Info/TeamInfo.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file)); 
+         
+        File file2 = new File("Company Info/CompanyInfo.txt");
+        BufferedReader br2 = new BufferedReader(new FileReader(file2)); 
         
-        int a = 5;
-        int b = 6; 
-        int c = a+b;
-        System.out.println(c);
+        String st; 
+        while ((st = br.readLine()) != null) 
+            System.out.println(st); 
+
+        System.out.println("");
+        
+        String st2; 
+        while ((st2 = br2.readLine()) != null) 
+            System.out.println(st2);         
     }
 
 }
